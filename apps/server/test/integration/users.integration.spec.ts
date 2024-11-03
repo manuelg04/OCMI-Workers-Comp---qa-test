57536895bb3a4572d678905666d5c360f806c20f
manuelg04 <manuelg9704@gmail.com>
2024-11-03 15:45:44 -0500

    Enhance user integration tests
    Added tests for new user registration, user updates, and error handling, including 404 errors for non-existent users, 500 errors for server issues, and 422 errors for invalid data. Improved test descriptions and updated token usage to "Bearer" format.

--- apps/server/test/integration/users.integration.spec.ts
+++ apps/server/test/integration/users.integration.spec.ts
@@ -1,11 +1,8 @@
-﻿// tests/integration/users.integration.spec.ts
-
-import request from 'supertest';
+﻿import request from 'supertest';
 import { makeExpressApp } from '../../src/lib';
 import { userRepository, sessionRepository } from '../../src/database';
 import { createMockUser, createMockSession } from '../helpers/mockData';
 import { User, Session } from '../../../../packages/shared/src/types';
-
 describe('Users API', () => {
   const app = makeExpressApp();
   let mockUser: User;
@@ -17,6 +14,7 @@ describe('Users API', () => {
 
     jest.spyOn(userRepository, 'find').mockResolvedValue(mockUser);
     jest.spyOn(userRepository, 'update').mockResolvedValue(mockUser);
+    jest.spyOn(userRepository, 'register').mockResolvedValue(mockUser);
     jest.spyOn(sessionRepository, 'create').mockResolvedValue(mockSession);
     jest
       .spyOn(sessionRepository, 'findByToken')
@@ -26,11 +24,12 @@ describe('Users API', () => {
   });
 
   afterEach(() => {
+    jest.clearAllMocks();
     jest.restoreAllMocks();
   });
 
   describe('POST /users', () => {
-    it('debería registrar un nuevo usuario y crear una sesión', async () => {
+    it('must register a new user and create a session', async () => {
       const newUser = {
         username: 'newuser',
         password: 'newpassword123',
@@ -42,6 +41,10 @@ describe('Users API', () => {
       };
 
       jest.spyOn(userRepository, 'register').mockResolvedValue(expectedUser);
+      jest.spyOn(sessionRepository, 'create').mockResolvedValue({
+        ...mockSession,
+        userId: expectedUser.id,
+      });
 
       const response = await request(app).post('/users').send(newUser);
 
@@ -49,6 +52,7 @@ describe('Users API', () => {
 
       const expectedSession = {
         ...mockSession,
+        userId: expectedUser.id,
         createdAt: mockSession.createdAt.toISOString(),
       };
 
@@ -59,12 +63,12 @@ describe('Users API', () => {
   });
 
   describe('GET /users/:userId', () => {
-    it('debería obtener un usuario por ID', async () => {
+    it('must get a user by id', async () => {
       const userId = mockUser.id;
 
       const response = await request(app)
         .get(`/users/${userId}`)
-        .set('Authorization', mockSession.token);
+        .set('Authorization', `Bearer ${mockSession.token}`);
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual({
@@ -72,6 +76,94 @@ describe('Users API', () => {
         favoriteBook: undefined,
       });
       expect(userRepository.find).toHaveBeenCalledWith(userId);
+    });
+
+    it('must return a 404 error if the user does not exist', async () => {
+      const userId = 'non-existent-id';
+      jest
+        .spyOn(userRepository, 'find')
+        .mockImplementation(async () => {
+          throw new Error('User not found');
+        });
+
+      const response = await request(app)
+        .get(`/users/${userId}`)
+        .set('Authorization', `Bearer ${mockSession.token}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({ message: 'User not found' });
+    });
+
+  });
+
+  describe('PUT /users/:userId', () => {
+    it('must update the user information', async () => {
+      const userId = mockUser.id;
+      const updatedData = {
+        username: 'updatedUsername',
+        favoriteBook: {
+          key: 'OL123M',
+          title: 'Example Book Title',
+          author_name: ['Author One', 'Author Two'],
+          first_publish_year: 1999,
+        },
+      };
+
+      const updatedUser = {
+        ...mockUser,
+        ...updatedData,
+        favoriteBook: JSON.stringify(updatedData.favoriteBook),
+      };
+
+
+      jest.spyOn(userRepository, 'find').mockResolvedValue(mockUser);
+      jest.spyOn(userRepository, 'update').mockResolvedValue(updatedUser);
+
+      const response = await request(app)
+        .put(`/users/${userId}`)
+        .set('Authorization', `Bearer ${mockSession.token}`)
+        .send(updatedData);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(updatedUser);
+    });
+
+    it('must return a 500 error if the user does not exist', async () => {
+      const userId = 'non-existent-id';
+      const updatedData = {
+        username: 'updatedUsername',
+        favoriteBook: 'New Favorite Book',
+      };
+
+      jest
+        .spyOn(userRepository, 'find')
+        .mockRejectedValue(new Error('User not found'));
+
+      const response = await request(app)
+        .put(`/users/${userId}`)
+        .set('Authorization', `Bearer ${mockSession.token}`)
+        .send(updatedData);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({ message: 'Internal server error' });
+    });
+
+    it('must return a 422 error if the data is invalid', async () => {
+      const invalidData = {
+        username: '',
+        favoriteBook: {
+          key: '',
+          title: '',
+        },
+      };
+
+      const response = await request(app)
+        .put(`/users/${mockUser.id}`)
+        .set('Authorization', `Bearer ${mockSession.token}`)
+        .send(invalidData);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toHaveProperty('errors');
     });
   });
 });
