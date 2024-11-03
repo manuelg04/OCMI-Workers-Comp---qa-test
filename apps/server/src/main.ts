77f19f718762591d1d89ebc9dd76cb644d7918ad
CristianHG2 <cherreragiraldo@gmail.com>
2024-11-03 08:48:50 -0500

    chore(qa-assessment): initial commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,23 @@
-import express from 'express';
+import {
+  appLog,
+  dbLog,
+  makeExpressApp,
+  migrateDb,
+  sqlite,
+  verifyTestData,
+} from './lib';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
+const app = makeExpressApp();
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  appLog(`Server started on http://${host}:${port}`);
+
+  sqlite()
+    .then(() => dbLog('Database initialized, verifying migrations...'))
+    .then(() => migrateDb())
+    .then(() => dbLog('Migrations up to date'))
+    .then(() => verifyTestData());
 });
